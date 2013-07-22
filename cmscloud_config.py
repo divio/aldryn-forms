6430c307e8cb8b2bf0db21b65e096e11443e5ec9
Krzysiek Szularz <szuliq@gmail.com>
2013-07-22 15:32:36 +0200

    Add cloud config for recaptcha keys

---
+++ cmscloud_config.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from cmscloud_client import forms
+
+
+class Form(forms.BaseForm):
+
+    public_key = forms.CharField('Re-captcha public key', max_length=40, required=False)
+    private_key = forms.CharField('Re-captcha private key', max_length=40, required=False)
+
+    def to_settings(self, data, settings):
+        settings['RECAPTCHA_PUBLIC_KEY'] = data['public_key']
+        settings['RECAPTCHA_PRIVATE_KEY'] = data['private_key']
+        return settings
