654c63873438533b82df12044315f4972ace435f
Kim Thoenen <kim@smuzey.ch>
2014-05-26 12:11:05 +0200

    fix aldryn config

--- aldryn_config.py
+++ aldryn_config.py
@@ -6,7 +6,7 @@ class Form(forms.BaseForm):
 
     public_key = forms.CharField('Re-captcha public key', max_length=40, required=False)
     private_key = forms.CharField('Re-captcha private key', max_length=40, required=False)
-    show_all_recipients = forms.BooleanField('Show all users in form recipients select', default=False, required=False)
+    show_all_recipients = forms.CheckboxField('Show all users in form recipients select', required=False)
 
     def to_settings(self, data, settings):
         settings['RECAPTCHA_PUBLIC_KEY'] = data['public_key']
