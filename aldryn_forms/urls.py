d986e5f33033727fec1641b589b140ba863d7c3d
Paulo <commonzenpython@gmail.com>
2014-08-22 23:49:28 -0400

    Change url name to aldryn_forms_submit_form. This url is never used explicitely, but should note that it's backwards incompatible.

--- aldryn_forms/urls.py
+++ aldryn_forms/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import patterns, url
 
 
 urlpatterns = patterns('aldryn_forms.views',
-    url(r'^$', 'submit_form_view', name='send'),
+    url(r'^$', 'submit_form_view', name='aldryn_forms_submit_form'),
 )
