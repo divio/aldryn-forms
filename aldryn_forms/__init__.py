92c6f6a3ae02e3a806bf187953c3c1fe0661e873
Paulo <commonzenpython@gmail.com>
2015-12-07 19:01:19 -0500

    adds deprecation warning for FormData model

--- aldryn_forms/__init__.py
+++ aldryn_forms/__init__.py
@@ -1,2 +1,10 @@
 # -*- coding: utf-8 -*-
+import warnings
+
 __version__ = '1.0.1'
+
+warnings.warn(
+    'The "aldryn_forms.FormData" model has been deprecated in '
+    'favor of aldryn_forms.FormSubmission',
+    DeprecationWarning
+)
