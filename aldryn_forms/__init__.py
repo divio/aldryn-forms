fe86a37d3c04cb06acd1f4649124144b77e1c6f4
Stefan Foulis <stefan@foulis.ch>
2016-02-18 13:47:49 +0100

    remove DeprecationWarning that was always shown
    The point of DeprecationWarnings is to show a warning if someone tries
    to use a feature that is deprecated. But this warning would always show
    up.
    Besides, aldryn_forms.models.FormData has since been removed.

--- aldryn_forms/__init__.py
+++ aldryn_forms/__init__.py
@@ -1,10 +1,2 @@
 # -*- coding: utf-8 -*-
-import warnings
-
 __version__ = '2.1.0'
-
-warnings.warn(
-    'The "aldryn_forms.FormData" model has been deprecated in '
-    'favor of aldryn_forms.FormSubmission',
-    DeprecationWarning
-)
