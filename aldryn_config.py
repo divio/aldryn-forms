d375dd14a61e5df4e351f6a941463f50c248467b
Stefan Foulis <stefan@foulis.ch>
2016-02-18 12:11:28 +0100

    Clearer show_all_recipients label
    and now defaults to showing all recipients.

--- aldryn_config.py
+++ aldryn_config.py
@@ -4,8 +4,8 @@ from aldryn_client import forms
 
 class Form(forms.BaseForm):
 
-    show_all_recipients = forms.CheckboxField('Include only admin users as selectable e-mail recipients instead of everyone.', required=False)
+    show_all_recipients = forms.CheckboxField('Show all users as selectable e-mail recipients (not only admins)', required=False, initial=True)
 
     def to_settings(self, data, settings):
-        settings['ALDRYN_FORMS_SHOW_ALL_RECIPIENTS'] = not data['show_all_recipients']
+        settings['ALDRYN_FORMS_SHOW_ALL_RECIPIENTS'] = data['show_all_recipients']
         return settings
