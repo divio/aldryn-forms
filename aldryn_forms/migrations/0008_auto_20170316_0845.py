620d7efdb638b6d4fd9d9d0f499dbd0c6aa73242
Stefan Foulis <stefan@foulis.ch>
2017-03-16 08:46:26 +0100

    Allow fields with more than 255 chars
    The max_value on each form field type is used for max length on all text
    types and for max amount of selections on multi choice fields. It was 
    limited to 255 due to the previous way how submitted form data was
    saved. But that limit does not make sense for a TextArea, which should
    not have a limit.

---
+++ aldryn_forms/migrations/0008_auto_20170316_0845.py
@@ -0,0 +1,39 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('aldryn_forms', '0007_auto_20170315_1421'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='emailfieldplugin',
+            name='max_value',
+            field=models.PositiveIntegerField(null=True, verbose_name='Max value', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='fieldplugin',
+            name='max_value',
+            field=models.PositiveIntegerField(null=True, verbose_name='Max value', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='fileuploadfieldplugin',
+            name='max_value',
+            field=models.PositiveIntegerField(null=True, verbose_name='Max value', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='imageuploadfieldplugin',
+            name='max_value',
+            field=models.PositiveIntegerField(null=True, verbose_name='Max value', blank=True),
+        ),
+        migrations.AlterField(
+            model_name='textareafieldplugin',
+            name='max_value',
+            field=models.PositiveIntegerField(null=True, verbose_name='Max value', blank=True),
+        ),
+    ]
