e31c02920503b54aa97b274d80650555be63bb59
Filipe Waitman <filwaitman@gmail.com>
2018-02-06 18:11:55 -0200

    Add missing migrations

--- aldryn_forms/migrations/0009_auto_20171218_1049.py
+++ aldryn_forms/migrations/0009_auto_20171218_1049.py
@@ -3,6 +3,10 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
+import aldryn_forms.utils
+
+action_backends_choices = aldryn_forms.utils.action_backend_choices()
+action_backends_choices = [(choice[0], str(choice[1])) for choice in action_backends_choices]
 
 
 class Migration(migrations.Migration):
@@ -15,6 +19,6 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='formplugin',
             name='storage_backend',
-            field=models.CharField(choices=[], default='default', max_length=15, verbose_name='Storage backend'),
+            field=models.CharField(choices=action_backends_choices, default='default', max_length=15, verbose_name='Action backend'),
         ),
     ]
