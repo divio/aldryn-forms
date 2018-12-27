6662c20f83437bfeb6f36a9de080ed05014aac32
Angelo Dini <angelo.dini@divio.ch>
2018-12-27 15:52:10 +0100

    adapted urlresolvers

--- aldryn_forms/views.py
+++ aldryn_forms/views.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from django.core.urlresolvers import resolve
+from django.urls import resolve
 from django.http import HttpResponseRedirect, HttpResponseBadRequest
 from django.shortcuts import render
 
