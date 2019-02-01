2d06b252e232a5f32208c57c8748ad5ab7c2c4a8
vThaian <b.plociennik@merixstudio.com>
2019-02-01 13:28:27 +0100

    Adapt forms with new changes

--- setup.py
+++ setup.py
@@ -6,29 +6,20 @@ from aldryn_forms import __version__
 
 
 REQUIREMENTS = [
-    'aldryn-boilerplates>=0.8.0',
-    'django-cms>=3.4.5',
-    'django-emailit>=0.2.4',
-    'djangocms-text-ckeditor>=3.7.0',
-    'djangocms-attributes-field>=1.0.0',
-    'django-simple-captcha',
-    'django-tablib',
-    'tablib',
-    'pillow',
-    'django-filer>=1.4.2',
-    'openpyxl<=2.4.9',  # 2.5.0b1 is raising "ImportError: cannot import name '__version__'"
-    'Django>=1.11',
-    'six>=1.0',
+    'django-cms>=3.5',
+    'django-appconf>=1.0,<2',
+    'django-extended-choices',
+    'djangocms-transfer',
+    'pygments',
+    'yurl',
+    'requests',
+    'celery>=3.0,<4.0',  # aldryn-celery supports only 3.X
 ]
+
 
 CLASSIFIERS = [
     'Development Status :: 5 - Production/Stable',
     'Environment :: Web Environment',
-
-    'Framework :: Django',
-    'Framework :: Django :: 1.11',
-    'Framework :: Django :: 2.0',
-    'Framework :: Django :: 2.1',
     'Intended Audience :: Developers',
     'License :: OSI Approved :: BSD License',
     'Operating System :: OS Independent',
