3be3720bc73a537a3691965318b2c51347ff31fc
useHTML <mne@sait-nado.ru>
2019-12-10 15:40:19 +0500

    disable csrf for speed

--- aldryn_forms/ajax_urls.py
+++ aldryn_forms/ajax_urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
 from .views import AjaxSubmit
-
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('', AjaxSubmit.as_view(), name='aldryn_forms_ajax_submit_form'),
+    path('', csrf_exempt(AjaxSubmit.as_view()), name='aldryn_forms_ajax_submit_form'),
 ]
