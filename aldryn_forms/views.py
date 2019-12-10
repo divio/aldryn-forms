e27f2d850db50dde2c70d436385ed2c1f11c41a6
useHTML <mne@sait-nado.ru>
2019-12-10 16:20:21 +0500

    disable form after submit

--- aldryn_forms/views.py
+++ aldryn_forms/views.py
@@ -21,7 +21,12 @@ class AjaxSubmit(FormView):
     # template_name = 'aldryn_forms/form.html'
 
     def post(self, request, *args, **kwargs):
-        cms_page = get_page_from_path(get_current_site(request), request.POST.get('page_path', '')[1:])
+        page_path = request.POST.get('page_path', '')
+        if page_path == '/':
+            page_path = ''
+        else:
+            page_path = page_path[1:-1]
+        cms_page = get_page_from_path(get_current_site(request), page_path)
         if not cms_page:
             return HttpResponseBadRequest('no cms_page')
         context = {
