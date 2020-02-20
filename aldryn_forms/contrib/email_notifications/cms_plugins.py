8b68515e87a34e7c801a3d21df2af92cb97815bd
Vladislav Yena <vladislav@what.digital>
2020-02-20 19:57:48 +0200

    Fix the HTML rendering function in the Form popup

--- aldryn_forms/contrib/email_notifications/cms_plugins.py
+++ aldryn_forms/contrib/email_notifications/cms_plugins.py
@@ -4,7 +4,7 @@ from email.utils import parseaddr
 
 from django.contrib import admin
 from django.core.mail import get_connection
-from django.utils.html import format_html
+from django.utils.html import mark_safe
 from django.utils.translation import ugettext_lazy as _
 
 from cms.plugin_pool import plugin_pool
@@ -112,7 +112,7 @@ class ExistingEmailNotificationInline(admin.StackedInline):
                 li_items.append(li_item)
         unordered_list = '<ul>{0}</ul>'.format(''.join(li_items))
         help_text = '<p class="help">{0}</p>'.format(self.text_variables_help_text)
-        return format_html(unordered_list + '\n' + help_text)
+        return mark_safe(unordered_list + '\n' + help_text)
     text_variables.allow_tags = True
     text_variables.short_description = _('available text variables')
 
